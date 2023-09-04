ce0baf9df3202b74fe3911d31998373834a94c10
RafaelBenitez <rafaelbenitezdaurtesharp@gmail.com>
2023-09-04 13:09:56 -0400

    primeiro commit

---
+++ RazorPagesFilmes/Pages/Filmes/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesFilmes.Modelo;
+using Microsoft.AspNetCore.Mvc;
+namespace RazorPagesFilmes.Pages.Filmes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesFilmes.Data.RazorPagesFilmesContext _context;
+
+        public IndexModel(RazorPagesFilmes.Data.RazorPagesFilmesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Filme> Filme { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string TermoBusca { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string FilmeGenero { get; set; }
+
+        public SelectList Generos { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var filmes = from m in _context.Filme
+                         select m;
+
+            if (!string.IsNullOrWhiteSpace(TermoBusca))
+            {
+                filmes = filmes.Where(f => f.Titulo.Contains(TermoBusca));
+            }
+
+            if (!string.IsNullOrWhiteSpace(FilmeGenero))
+            {
+                filmes = filmes.Where(f => f.Genero == FilmeGenero);
+            }
+
+            Generos = new SelectList(await _context.Filme.Select(o => o.Genero).Distinct().ToListAsync());
+            Filme = await filmes.ToListAsync();
+        }
+    }
+}
